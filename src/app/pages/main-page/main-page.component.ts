5f99949ad3c2ad39c36ddcd580799da19d19af27
Darren Sim <klassiclegend@gmail.com>
2021-06-20 15:34:53 +0800

    Update main page

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPage {
-  constructor(private router: Router) {}
-
-  routeToLogin(): void {
-    this.router.navigate(['/login']);
-  }
+  constructor() {}
 }
