7a19c614c9213c5a71934342fb32bbc4a85b4688
Darren Sim <klassiclegend@gmail.com>
2021-06-23 00:13:42 +0800

    Style create event page

--- src/app/pages/create-event-page/create-event-page.component.ts
+++ src/app/pages/create-event-page/create-event-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-event-page',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-event-page.component.scss'],
 })
 export class CreateEventPage implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  routeToMyEventsPage(): void {
+    this.router.navigate(['/my-events']);
+  }
 }
