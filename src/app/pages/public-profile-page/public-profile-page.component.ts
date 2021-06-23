876bbbf6de5947a6f8a31bd927579f56f6539354
Darren Sim <klassiclegend@gmail.com>
2021-06-23 13:17:51 +0800

    Write in public profile page

--- src/app/pages/public-profile-page/public-profile-page.component.ts
+++ src/app/pages/public-profile-page/public-profile-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Subscription } from 'rxjs';
-import { UserProfileDetails } from 'src/app/models/data/profile';
+import { UserProfile } from 'src/app/models/data/profile';
 import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
@@ -13,7 +13,7 @@ import { ProfileService } from 'src/app/services/profile.service';
 export class PublicProfilePage implements OnInit, OnDestroy {
   private userId: string | null = '';
   subscriptions: Subscription[] = [];
-  profileDetails: UserProfileDetails = <UserProfileDetails>{};
+  profileDetails: UserProfile = <UserProfile>{};
 
   constructor(
     private profileService: ProfileService,
@@ -27,6 +27,7 @@ export class PublicProfilePage implements OnInit, OnDestroy {
       this.profileService.getUserProfileById(this.userId ? this.userId : '').subscribe(
         (res) => {
           this.profileDetails = { ...res.data.user };
+          console.log(this.profileDetails);
         },
         (err) => {
           this.messageService.add({
