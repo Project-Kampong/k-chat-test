84721ccc4bc576d7a40b038584fb0c298933e322
Darren Sim <klassiclegend@gmail.com>
2021-06-23 12:53:49 +0800

    Write in organized event page

--- src/app/pages/organized-event-page/organized-event-page.component.ts
+++ src/app/pages/organized-event-page/organized-event-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { OrganizedEvent } from 'src/app/models/data/organizedEvents';
+import { OrganizedEventsService } from 'src/app/services/organized-events.service';
 
 @Component({
   selector: 'app-organized-event-page',
@@ -6,7 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organized-event-page.component.scss'],
 })
 export class OrganizedEventPage implements OnInit {
-  constructor() {}
+  subscriptions: Subscription[] = [];
+  private eventId: string | null = '';
+  eventDetails: OrganizedEvent = <OrganizedEvent>{};
 
-  ngOnInit(): void {}
+  constructor(
+    private organizedEventsService: OrganizedEventsService,
+    private route: ActivatedRoute,
+    private messageService: MessageService,
+  ) {}
+
+  ngOnInit(): void {
+    this.eventId = this.route.snapshot.paramMap.get('id') ? this.route.snapshot.paramMap.get('id') : '';
+    this.subscriptions.push(
+      this.organizedEventsService.getEventById(this.eventId ? this.eventId : '').subscribe(
+        (res) => {
+          this.eventDetails = { ...res.data.organizedEvent };
+          console.log(this.eventDetails);
+        },
+        (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Something went wrong :(',
+            detail: 'Please reload the page again.',
+          });
+        },
+      ),
+    );
+  }
+
+  routeToQNAPage(): void {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Q&A Session is closed',
+      detail: "Oops! This function isn't available at the moment. :(",
+    });
+  }
 }
